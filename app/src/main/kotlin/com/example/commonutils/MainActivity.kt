0bc2c7be971fca43f58f75bf8ab3185c1467cf00
“Chien <phamquangchien170499@gmail.com>
2023-01-18 17:42:57 +0700

    update gradle

--- app/src/main/kotlin/com/example/commonutils/MainActivity.kt
+++ app/src/main/kotlin/com/example/commonutils/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.commonutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvMain = findViewById<TextView>(R.id.tvMain)
+//        tvMain.setEllipsizedSuffix()
     }
 }
