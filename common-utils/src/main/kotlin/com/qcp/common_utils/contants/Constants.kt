bc3790d0688acb81d775cd8eb48d9111a8973603
“Chien <phamquangchien170499@gmail.com>
2023-01-18 17:15:07 +0700

    Initial commit and add cut text by character

---
+++ common-utils/src/main/kotlin/com/qcp/common_utils/contants/Constants.kt
@@ -0,0 +1,25 @@
+package com.qcp.common_utils.contants
+
+/**
+ * @author   Quang Chien Pham <https://github.com/quangchien99>
+ * @since    18/01/2023
+ */
+
+/**
+ * Declaration of constants used for this module
+ */
+object Constants {
+
+    /**
+    Constants used commonly
+     */
+    const val SPACE_VALUE = " "
+    const val EMPTY_VALUE = ""
+
+    /**
+    Constants used for TextViewUtils
+     */
+    const val DEFAULT_MAX_LINE = 1
+    const val DEFAULT_SUFFIX = "..."
+
+}
---
+++ common-utils/src/main/kotlin/com/qcp/common_utils/textview/TextViewUtils.kt
@@ -0,0 +1,86 @@
+package com.qcp.common_utils.textview
+
+import android.graphics.Paint
+import android.view.View
+import android.widget.TextView
+import com.qcp.common_utils.contants.Constants.DEFAULT_MAX_LINE
+import com.qcp.common_utils.contants.Constants.DEFAULT_SUFFIX
+import com.qcp.common_utils.contants.Constants.SPACE_VALUE
+
+/**
+ * @author   Quang Chien Pham <https://github.com/quangchien99>
+ * @since    18/01/2023
+ */
+object TextViewUtils {
+
+    /** Make the content of the text view ellipsized with maxLines and suffix defined.
+     * @param maxLines the line applied ellipsize.
+     * @param suffix the suffix when ellipsized.
+     */
+    fun TextView.setEllipsizedSuffix(
+        maxLines: Int = DEFAULT_MAX_LINE,
+        suffix: String = DEFAULT_SUFFIX
+    ) {
+        addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
+            override fun onLayoutChange(
+                v: View?,
+                left: Int,
+                top: Int,
+                right: Int,
+                bottom: Int,
+                oldLeft: Int,
+                oldTop: Int,
+                oldRight: Int,
+                oldBottom: Int
+            ) {
+
+                val allText = text.toString()
+                var newText = allText
+                val tvWidth = width
+                val textSize = textSize
+                if (!textHasEllipsized(newText, tvWidth, textSize, maxLines)) return
+
+                while (textHasEllipsized(newText, tvWidth, textSize, maxLines)) {
+                    newText = newText.substring(0, newText.length - 1).trim()
+                }
+
+                // now replace the last few chars with the suffix if we can
+
+                var endIndex =
+                    newText.length - suffix.length - 2
+                if (endIndex > 0) {
+                    if (newText[endIndex].toString() == SPACE_VALUE) {
+                        newText[endIndex]
+                        newText = "${newText.substring(0, endIndex).trim()}$suffix"
+                    } else {
+                        while (newText[endIndex - 1].toString() != SPACE_VALUE) {
+                            endIndex -= 1
+                        }
+                        newText[endIndex]
+                        newText = "${newText.substring(0, endIndex).trim()}$suffix"
+                    }
+                }
+
+                text = newText
+
+                removeOnLayoutChangeListener(this)
+            }
+        })
+    }
+
+    /**
+     * To determine the text view has been ellipsized or not
+     */
+    private fun textHasEllipsized(
+        text: String,
+        tvWidth: Int,
+        textSize: Float,
+        maxLines: Int
+    ): Boolean {
+        val paint = Paint()
+        paint.textSize = textSize
+        val size = paint.measureText(text).toInt()
+
+        return size > tvWidth * maxLines
+    }
+}
