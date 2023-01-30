eecc8570d33f2a89e4b2b71e37b833a6a49f90a4
Quang Chien <69864173+quangchien99@users.noreply.github.com>
2023-01-30 15:58:48 +0700

    update version 1.0.8 (#1)
    Co-authored-by: “Chien <“phamquangchien170499@gmail.com”>

--- app/src/main/kotlin/com/example/commonutils/MainActivity.kt
+++ app/src/main/kotlin/com/example/commonutils/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.commonutils
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.commonutils.databinding.ActivityMainBinding
+import com.example.commonutils.ellipsize.EllipsizeTextViewActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tvMain = findViewById<TextView>(R.id.tvMain)
-//        tvMain.setEllipsizedSuffix()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEllipsize.setOnClickListener {
+            startActivity(
+                Intent(this, EllipsizeTextViewActivity::class.java)
+            )
+        }
     }
 }
